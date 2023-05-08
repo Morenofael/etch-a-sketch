7bb8eba8db537724364b9690bc27b607ee2ab2c0
Rafael Moreno <rafaelchecchia@proton.me>
2023-05-08 12:49:34 -0300

    Inicio de projeto

---
+++ code.js
@@ -0,0 +1,6 @@
+for(i=0;i<16;i++){
+    let main = document.querySelector('main')
+    let rowDiv = document.createElement('div');
+    rowDiv.style.height = '6.25vh';
+    main.appendChild(rowDiv);
+}
