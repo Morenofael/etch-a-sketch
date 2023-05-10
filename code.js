a7fc23548ff5ca0408f6800a7c0295274d2a309c
Rafael Moreno <rafaelchecchia@proton.me>
2023-05-09 21:24:51 -0300

    adicionar marcador permanente

--- code.js
+++ code.js
@@ -5,6 +5,7 @@ for(i=0;i<16;i++){
     for(j=0;j<16;j++){
         let squareDiv = document.createElement('div');
         squareDiv.classList = 'squareDiv';
+        squareDiv.addEventListener("mouseover",function(){squareDiv.style.backgroundColor='black'});
         rowDiv.appendChild(squareDiv);
     }
     main.appendChild(rowDiv);
